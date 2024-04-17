ab385cca01987327ef58a3613559f3701670b1e5
John Fuentes <jmcaluyafuentes@gmail.com>
2024-04-18 09:32:22 +1000

    Finished python control flow lesson

---
+++ python/control-flow/compare.py
@@ -0,0 +1,21 @@
+# x = int(input('What is X? '))
+# y = int(input('What is Y? '))
+
+# if x < y:
+#     print('x is less than y')
+
+# print('Done')
+
+# score = int(input('Score (0-100): '))
+
+# if score >= 90:
+#     print('HD')
+# elif score >= 80:
+#     print('D')
+# elif score >= 70:
+#     print('CR')
+# elif score >= 50:
+#     print('P')
+# else:
+#     print('F')
+
