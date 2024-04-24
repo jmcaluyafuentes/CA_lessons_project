e55fdc1eff56c2370d9863410f5f417e4f1a5b65
John Fuentes <jmcaluyafuentes@gmail.com>
2024-04-24 17:01:24 +1000

    Finished lesson python on iterators

--- python/data-structures/iterators.py
+++ python/data-structures/iterators.py
@@ -1,4 +1,5 @@
 nums = [10, 14, 21, 50, 5, -6]
+spam = ['cat', 'dog']
 
 def square(x):
     return x ** 2
@@ -6,8 +7,40 @@ def square(x):
 def cube(x):
     return x ** 3
 
-squared_num = list(map(square, nums))
-cubed_num = list(map(cube, nums))
+squared_nums = list(map(square, nums))
+even_numbers = list(filter(lambda x: x % 2 == 0, nums))
+cubed_nums = list(map(lambda x: x ** 3, even_numbers))
 
-print(squared_num)
-print(cubed_num)
+# print(squared_nums)
+# print(cubed_nums)
+# print(even_numbers)
+
+print(sorted(nums))
+print(sorted(nums, reverse=True))
+
+########################################################
+# Sorting a dictionary
+
+employees = [
+    {'name': 'John', 'age': 36},
+    {'name': 'Mary', 'age': 27},
+    {'name': 'Bob', 'age': 40}
+]
+
+print(sorted(employees, key=lambda emp: emp['name']))
+
+print(sorted(employees, key=lambda emp: emp['age']))
+
+print(max(employees, key=lambda emp: emp['age']))
+
+print(max(employees, key=lambda emp: emp['age'])['age'])
+
+########################################################
+# Alternative way
+# To get the max of the ages
+# First get the list of ages
+
+print(list(map(lambda emp: emp['age'], employees)))
+
+# Then get the max
+print(max(map(lambda emp: emp['age'], employees)))
