3bfb352f62e712162204815b2dee3e7d69b8676c
John Fuentes <jmcaluyafuentes@gmail.com>
2024-05-30 13:24:45 +1000

    Finished Flask ORM part 2

--- 05_flask/02_trello/app.py
+++ 05_flask/02_trello/app.py
@@ -1,23 +1,46 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String, Text
+from typing import Optional
+from flask_marshmallow import Marshmallow
+
+class Base(DeclarativeBase):
+    pass
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:trello_dev@localhost:5432/trello'
 
-db = SQLAlchemy(app)
+# Using the old version of SQLAlchemy
+# db = SQLAlchemy(app)
 # print(vars(db))
+
+# Using the new version of SQLAlchemy
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+ma = Marshmallow(app)
 
 'Declare a model'
 class Card(db.Model):
     __tablename__ = 'cards' # Use table name 'cards' to follow the convention.
 
-    id = db.Column(db.Integer, primary_key=True)
+    # id = db.Column(db.Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True) #Call the mapped_column if there is/are constraints
 
-    title = db.Column(db.String(100))
-    description = db.Column(db.Text())
-    date_created = db.Column(db.Date())
+    # title = db.Column(db.String(100))
+    title: Mapped[str] = mapped_column(String(100))
+
+    # description = db.Column(db.Text())
+    description: Mapped[Optional[str]] = mapped_column(Text)
+
+    # date_created = db.Column(db.Date())
+    date_created: Mapped[date]
+
+class CardSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'title', 'description', 'date_created')
 
 @app.cli.command('db_create')
 def db_create():
@@ -43,7 +66,8 @@ def db_create():
 
     db.session.commit() # This is like the 'commit' in git
 
-@app.cli.command('all_cards')
+# @app.cli.command('all_cards')
+@app.route('/cards')
 def all_cards():
     # The purpose is to make a query using ORM equivalent to the sql command 'select * from cards;'
     # stmt = db.select(Card) # ORM works with the model and not directly to postgres table
@@ -78,16 +102,54 @@ def all_cards():
     # for card in cards:
     #     print(card.title)
 
-    stmt = db.select(Card).where(db.or_(Card.id < 3, Card.description != 'Done'))
+    # stmt = db.select(Card).where(db.or_(Card.id < 3, Card.description != 'Done'))
+    
+    # id = 2
+    # stmt = db.select(Card).where(Card.id == id)
+    # stmt = db.select(Card).filter_by(id = id) #Filter can be used if query if not complex
+    # stmt = db.select(Card).where(Card.id != id)
+    # stmt = db.select(Card).where(Card.description.like('%Implement%')) # Keyword like is for sub filter. % is a wildcard.
+    # stmt = db.select(Card).where(Card.description.like('%Implement%')).order_by(Card.title)
+    # stmt = db.select(Card).where(Card.description.like('%Implement%')).order_by(Card.title.desc()) # Descending order
     # print(stmt)
-    cards = db.session.scalars(stmt)
-    for card in cards:
-        print(card.title)
+
+    # cards = db.session.scalars(stmt)
+    # for card in cards:
+    #     print(card.title)
+
+    # foo = 2
+    # card = db.get_or_404(Card, foo)
+    # print(card)
+
+    # stmt = db.select(Card).where(Card.id < 2)
+    # card = db.one_or_404(stmt)
+    # print(card)
+
+    stmt = db.select(Card)
+    cards = db.session.scalars(stmt).all()
+    # for card in cards:
+    #     print(vars(card))
+    # return json.dumps(cards, default=lambda c: vars(c)) # This code did not work, therefore use Marsmallow
+    # print(CardSchema(many=True).dumps(cards))
+    # print(type(CardSchema(many=True).dumps(cards)))
+    # print(CardSchema(many=True).dump(cards))
+    # print(type(CardSchema(many=True).dump(cards)))
+
+    # return (CardSchema(many=True).dump(cards))
+    return (CardSchema(many=True, only=['id', 'title']).dump(cards))
+
+@app.route('/cards/<int:id>')
+def one_card(id):
+    card = db.get_or_404(Card, id)
+    return CardSchema().dump(card)
 
 @app.route('/')
 def index():
     return 'Hello world'
 
+@app.errorhandler(404)
+def not_found(err):
+    return {'error': f'{err}'}
+
 # if __name__ == '__main__':
 #     app.run(debug=True)
-
