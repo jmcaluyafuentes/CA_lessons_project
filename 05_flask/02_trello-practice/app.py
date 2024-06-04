5217441a6214f945ce90be46a8a8b48614013c22
John Fuentes <jmcaluyafuentes@gmail.com>
2024-06-04 14:27:58 +1000

    Practice on flask app

--- 05_flask/02_trello-practice/app.py
+++ 05_flask/02_trello-practice/app.py
@@ -3,6 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'Hello world!'
+def home():
+    return '<h1>Welcome to my first Flask app!</h1>'
 
+@app.errorhandler(404)
+def error(e):
+    return f'Error: {e}', 404
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
