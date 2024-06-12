56b4b35e59f0e52da7c08ae1369bc8e2a5bef349
John Fuentes <jmcaluyafuentes@gmail.com>
2024-06-12 13:31:44 +1000

    Added lines in cards model

--- 05_flask/03_trello-crud/models/card.py
+++ 05_flask/03_trello-crud/models/card.py
@@ -1,18 +1,26 @@
-from datetime import date
 from typing import Optional
+from datetime import date
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String, Text
 from init import db, ma
 
-'Declare a model'
-class Card(db.Model): # Card is arbitrary name
-    __tablename__ = 'cards' # Use table name 'cards' to follow the convention.
 
-    id: Mapped[int] = mapped_column(primary_key=True) #Call the mapped_column if there is/are constraints
+class Card(db.Model):
+    __tablename__ = "cards"
+
+    # id = db.Column(db.Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    # title = db.Column(db.String(100))
     title: Mapped[str] = mapped_column(String(100))
-    description: Mapped[Optional[str]] = mapped_column(Text) # By default all fields by default is Not Null value. Optional will make the attribute optional.
+    # description = db.Column(db.Text())
+    description: Mapped[Optional[str]] = mapped_column(Text())
+    # date_created = db.Column(db.Date())
     date_created: Mapped[date]
 
-class CardSchema(ma.Schema): # Marshmallow
+
+# Marshmallow schema (NOT a db schema!)
+# Used by Marshmallow to serialize and/or validate our SQLAlchemy models
+class CardSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'title', 'description', 'date_created')
+        fields = ("id", "title", "description", "date_created")
